cf6194cf04cfd56fb878de2c346b6c6a356f70f6
jjunnila <joakim.junnila99@gmail.com>
2024-10-08 14:07:20 +0300

    Part 3.8 complete

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+morgan.token('json', function getJson (req) {
+    if (req.method === "POST")
+        return JSON.stringify(req.body)
+    else 
+        return " "
+  })
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
 
 let persons = [
     {
