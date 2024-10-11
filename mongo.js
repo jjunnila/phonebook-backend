5d34ce9c31bd876a97fdf3ca31d4ba2a55778ed6
jjunnila <joakim.junnila99@gmail.com>
2024-10-11 13:45:08 +0300

    Part 3.12 complete

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://dbuser:${password}@fso.t3wfx.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=FSO`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+        id: Math.floor(Math.random() * 9999).toString()
+    })
+    person.save().then(result => {
+        console.log(`Added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    }) 
+} else {
+    Person.find({}).then(result => {
+        console.log("Phonebook: ")
+        result.forEach(person => {
+            console.log(person.name, " ", person.number)
+        })
+        mongoose.connection.close()
+    })
+}
